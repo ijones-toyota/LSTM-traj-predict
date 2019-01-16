a3a5db16ec35e8e4bba396f1ffe5edeaa8dc3d7a
Ian <ijones@us.toyota-itc.com>
2019-01-16 15:58:16 -0800

    added analysis, about to output to file

--- src/analysis/error.py
+++ src/analysis/error.py
@@ -11,32 +11,27 @@ import math
 # Returns:
 #   List of lists
 #   [0] simulated headway distance
-#   [1] simulated relative velocity differential
-#   [2] simulated velocity
-#   [3] simulated acceleration
-#   [4] true headway distance
-#   [5] true relative velocity differential
-#   [6] true velocity
-#   [7] true acceleration
+#   [1] simulated velocity
+#   [2] simulated acceleration
+#   [3] true headway distance
+#   [4] true velocity
+#   [5] true acceleration
 """
 def separateData(dataset):
 
     print "separating data..."
 
     sim_dist = []
-    sim_rel  = []
     sim_vel  = []
     sim_acc  = []
 
     true_dist = []
-    true_rel  = []
     true_vel  = []
     true_acc  = []
 
     for line in dataset:
         line = line.split(',')
         true_dist.append(float(line[200]))
-        true_rel.append(float(line[201]))
         true_vel.append(float(line[202]))
         true_acc.append(float(line[203]))
 
@@ -48,10 +43,6 @@ def separateData(dataset):
             if i % 4 == 0:
                 sim_dist.append(float(line[i]))
 
-            # Relative speed differential
-            elif (i-1) % 4 == 0:
-                sim_rel.append(float(line[i]))
-
             # Velocity
             elif (i-2) % 4 == 0:
                 sim_vel.append(float(line[i]))
@@ -60,7 +51,7 @@ def separateData(dataset):
             elif (i-3) % 4 == 0:
                 sim_acc.append(float(line[i]))
 
-    return sim_dist, sim_rel, sim_vel, sim_acc, true_dist, true_rel, true_vel, true_acc
+    return sim_dist, sim_vel, sim_acc, true_dist, true_vel, true_acc
 
 
 
@@ -72,44 +63,29 @@ def separateData(dataset):
 # Returns:
 #   List of lists
 #   [0] simulated headway distance
-#   [1] simulated relative velocity differential
-#   [2] simulated velocity
-#   [3] simulated acceleration
-#   [4] simulated follower headway distance
-#   [5] simulated follower velocity differential
-#   [6] true headway distance
-#   [7] true relative velocity differential
-#   [8] true velocity
-#   [9] true acceleration
-#   [10] true follow headway distance
-#   [11] true follow relative velocity differential
+#   [1] simulated velocity
+#   [2] simulated acceleration
+#   [3] true headway distance
+#   [4] true velocity
+#   [5] true acceleration
 """
 def separateFollowerData(dataset):
 
     print "separating data..."
 
     sim_dist = []
-    sim_rel  = []
     sim_vel  = []
     sim_acc  = []
-    sim_follow_dist = []
-    sim_follow_rel  = []
 
     true_dist = []
-    true_rel  = []
     true_vel  = []
     true_acc  = []
-    true_follow_dist = []
-    true_follow_rel  = []
 
     for line in dataset:
         line = line.split(',')
         true_dist.append(float(line[300]))
-        true_rel.append(float(line[301]))
         true_vel.append(float(line[302]))
         true_acc.append(float(line[303]))
-        true_follow_dist.append(float(line[304]))
-        true_follow_rel.append(float(line[305]))
 
         # Add list to keep track of samples for this trajectory
 
@@ -119,10 +95,6 @@ def separateFollowerData(dataset):
             if i % 6 == 0:
                 sim_dist.append(float(line[i]))
 
-            # Leader relative speed differential
-            elif (i-1) % 6 == 0:
-                sim_rel.append(float(line[i]))
-
             # Velocity
             elif (i-2) % 6 == 0:
                 sim_vel.append(float(line[i]))
@@ -131,22 +103,7 @@ def separateFollowerData(dataset):
             elif (i-3) % 6 == 0:
                 sim_acc.append(float(line[i]))
 
-            # Follower headway distance
-            elif (i-4) % 6 == 0:
-                sim_follow_dist.append(float(line[i]))
-
-            # Follower relative speed differential
-            elif (i-5) % 6 == 0:
-                sim_follow_rel.append(float(line[i]))
-
-    count = 0
-    for a in true_vel:
-        if a < 0:
-            count += 1
-            print(a)
-    print("total negative true velocities = " + str(count))
-
-    return sim_dist, sim_rel, sim_vel, sim_acc, sim_follow_dist, sim_follow_rel, true_dist, true_rel, true_vel, true_acc, true_follow_dist, true_follow_rel
+    return sim_dist, sim_vel, sim_acc, true_dist, true_vel, true_acc
 
 
 
@@ -158,17 +115,11 @@ def separateFollowerData(dataset):
 # Returns:
 #   List of lists
 #   [0] simulated headway distance
-#   [1] simulated relative velocity differential
-#   [2] simulated velocity
-#   [3] simulated acceleration
-#   [4] simulated follower headway distance
-#   [5] simulated follower velocity differential
-#   [6] true headway distance
-#   [7] true relative velocity differential
-#   [8] true velocity
-#   [9] true acceleration
-#   [10] true follow headway distance
-#   [11] true follow relative velocity differential
+#   [1] simulated velocity
+#   [2] simulated acceleration
+#   [3] true headway distance
+#   [4] true velocity
+#   [5] true acceleration
 """
 def separateNeighborsData(dataset):
 
@@ -203,13 +154,6 @@ def separateNeighborsData(dataset):
             # Acceleration
             elif (i-2) % 18 == 0:
                 sim_dist.append(float(line[i]))
-
-    count = 0
-    for a in true_vel:
-        if a < 0:
-            count += 1
-            print(a)
-    print("total negative true velocities = " + str(count))
 
     return sim_dist, sim_vel, sim_acc, true_dist, true_vel, true_acc
 
@@ -280,16 +224,13 @@ def calculateError(sim_vel, sim_acc, true_vel, true_acc):
 
     mrse_vel  = math.sqrt(1.0 * vel_err / len(sim_vel))
     mrse_acc  = math.sqrt(1.0 * acc_err / len(sim_vel))
-    print("Velocity Mean Root Squared Error     = %.4f" % mrse_vel)
-    print("Acceleration Mean Root Squared Error = %.4f" % mrse_acc)
+    print("Velocity MRSE = %.4f" % mrse_vel + ", Acceleration MRSE = %.4f" % mrse_acc)
 
     print("MRSE subdivided by 1-5 sec time horizon")
     for i in range(len(hor_vel_err)):
         hor_vel_err[i] /= (1.0 * total_hor_samples[i])
         hor_acc_err[i] /= (1.0 * total_hor_samples[i])
-        print("horizon " + str(i+1))
-        print("Velocity MRSE     = %.4f" % hor_vel_err[i])
-        print("Acceleration MRSE = %.4f" % hor_acc_err[i])
+        print(str(i+1) + " second horizon || Velocity MRSE = %.4f" % hor_vel_err[i] + " | Acceleration MRSE = %.4f" % hor_acc_err[i])
 
     return [mrse_vel, mrse_acc, hor_vel_err, hor_acc_err]
 
@@ -332,25 +273,36 @@ def sign(x):
 """
 # Loops over all acceleration trajectories and counts the number of jerk inversions
 """
-def countInversions(acc, type):
+def countInversions(true_acc, sim_acc):
 
-    j = jerk(acc)
-    n = len(j)
-    count = 0
+    ## Calculate true inversions ##
+    true_j = jerk(true_acc)
+    true_n = len(true_j)
+    true_count = 0
 
     # loop over trajectories
-    for i in range(0, n, 99):
+    for i in range(0, true_n, 99):
         # loop over jerks in trajectory, increment count if sign change
         for k in range(i+1, i+99):
-            if sign(j[k]) * sign(j[k-1]) < 0:
-                count += 1
+            if sign(true_j[k]) * sign(true_j[k-1]) < 0:
+                true_count += 1
 
-    if type == "True":
-        print("True inversions      = " + str(count * 99 / n))
-    elif type == "Simulated":
-        print("Simulated inversions = " + str(count * 99 / n))
 
-    return [count * 99, n]
+    ## Calculate simulated inversions ##
+    sim_j = jerk(sim_acc)
+    sim_n = len(sim_j)
+    sim_count = 0
+
+    # loop over trajectories
+    for i in range(0, sim_n, 99):
+        # loop over jerks in trajectory, increment count if sign change
+        for k in range(i+1, i+99):
+            if sign(sim_j[k]) * sign(sim_j[k-1]) < 0:
+                sim_count += 1
+
+    print("True inversions = " + str(true_count * 99 / true_n) + " | Simulated inversions = " + str(sim_count * 99 / sim_n))
+
+    return [true_count * 99, true_n, sim_count * 99, sim_n]
 
 
 
@@ -370,11 +322,9 @@ def countNegatives(sim_dist, sim_vel, true_dist):
         if elem < 0:
             num_neg_vel += 1
 
-    print("Raw number negative distance = " + str(num_neg_dist))
-    print("Raw number negative velocity = " + str(num_neg_vel))
+    print("Raw number negative distance = " + str(num_neg_dist) + " | Raw number negative velocity = " + str(num_neg_vel))
 
-    print("Frequency negative distance = %.4f" % (num_neg_dist / len(sim_dist)))
-    print("Frequency negative velocity = %.4f" % (num_neg_vel / len(sim_vel)))
+    print("Frequency negative distance = %.4f" % (num_neg_dist / len(sim_dist)) + " | Frequency negative velocity = %.4f" % (num_neg_vel / len(sim_vel)))
 
     return [num_neg_dist, num_neg_vel]
 
@@ -386,54 +336,64 @@ def countNegatives(sim_dist, sim_vel, true_dist):
 def printAnalysis(n_folds, vel_err, acc_err, hor_vel_err, hor_acc_err, num_true_inversions, total_true_inversion_traj,
                   num_sim_inversions, total_sim_inversion_traj, num_neg_dist, num_neg_vel, total_sim_traj):
 
+    # Error
+    mrse_vel, mrse_acc = (0,)*2
+    # Error for each time horizon
+    mrse_hor1_vel, mrse_hor1_acc, mrse_hor2_vel, mrse_hor2_acc, mrse_hor3_vel, mrse_hor3_acc, mrse_hor4_vel, mrse_hor4_acc, mrse_hor5_vel, mrse_hor5_acc = (0,)*10
+    # Negative headway, velocity
+    avg_neg_dist, avg_neg_vel, count_sim_traj = (0,)*3
+    # Inversions
+    avg_true_inversions, avg_sim_inversions, count_true_inversion_traj, count_sim_inversion_traj = (0,)*4
+
+
+    # Iterate over all folds, summing analytics for averaging
+    for i in range(n_folds):
+        mrse_vel += vel_err[i] / n_folds
+        mrse_acc += acc_err[i] / n_folds
+        mrse_hor1_vel += hor_vel_err[0][i] / n_folds
+        mrse_hor1_acc += hor_acc_err[0][i] / n_folds
+        mrse_hor2_vel += hor_vel_err[1][i] / n_folds
+        mrse_hor2_acc += hor_acc_err[1][i] / n_folds
+        mrse_hor3_vel += hor_vel_err[2][i] / n_folds
+        mrse_hor3_acc += hor_acc_err[2][i] / n_folds
+        mrse_hor4_vel += hor_vel_err[3][i] / n_folds
+        mrse_hor4_acc += hor_acc_err[3][i] / n_folds
+        mrse_hor5_vel += hor_vel_err[4][i] / n_folds
+        mrse_hor5_acc += hor_acc_err[4][i] / n_folds
+
+        avg_true_inversions += num_true_inversions[i]
+        avg_sim_inversions += num_sim_inversions[i]
+        count_true_inversion_traj += total_true_inversion_traj[i]
+        count_sim_inversion_traj += total_sim_inversion_traj[i]
+
+
+        avg_neg_dist += num_neg_dist[i]
+        avg_neg_vel += num_neg_vel[i]
+        count_sim_traj += total_sim_traj[i]
+
     print("\n\n")
     print("Analysis across all 10 folds:")
 
     # Display error across all folds
-    mrse_vel = vel_err / n_folds
-    mrse_acc = acc_err / n_folds
-    print("Average Velocity MRSE     = %.4f" % mrse_vel)
-    print("Average Acceleration MRSE = %.4f" % mrse_acc)
+    print("Avg Velocity MRSE = %.4f" % mrse_vel + " | Avg Acceleration MRSE = %.4f" % mrse_acc)
 
-    # Display error for each time horizon
-    mrse_hor1_vel = hor_vel_err[0] / n_folds
-    mrse_hor1_acc = hor_acc_err[0] / n_folds
-    mrse_hor2_vel = hor_vel_err[1] / n_folds
-    mrse_hor2_acc = hor_acc_err[1] / n_folds
-    mrse_hor3_vel = hor_vel_err[2] / n_folds
-    mrse_hor3_acc = hor_acc_err[2] / n_folds
-    mrse_hor4_vel = hor_vel_err[3] / n_folds
-    mrse_hor4_acc = hor_acc_err[3] / n_folds
-    mrse_hor5_vel = hor_vel_err[4] / n_folds
-    mrse_hor5_acc = hor_acc_err[4] / n_folds
+    # Display time horizon error across all folds
     print("Avg MRSE subdivided by 1-5 sec time horizon:")
-    print("1 second horizon:")
-    print("Avg Velocity MRSE     = %.4f" % mrse_hor1_vel)
-    print("Avg Acceleration MRSE = %.4f" % mrse_hor1_acc)
-    print("2 second horizon:")
-    print("Avg Velocity MRSE     = %.4f" % mrse_hor2_vel)
-    print("Avg Acceleration MRSE = %.4f" % mrse_hor2_acc)
-    print("3 second horizon:")
-    print("Avg Velocity MRSE     = %.4f" % mrse_hor3_vel)
-    print("Avg Acceleration MRSE = %.4f" % mrse_hor3_acc)
-    print("4 second horizon:")
-    print("Avg Velocity MRSE     = %.4f" % mrse_hor4_vel)
-    print("Avg Acceleration MRSE = %.4f" % mrse_hor4_acc)
-    print("5 second horizon:")
-    print("Avg Velocity MRSE     = %.4f" % mrse_hor5_vel)
-    print("Avg Acceleration MRSE = %.4f" % mrse_hor5_acc)
+    print("1 second horizon || Avg Velocity MRSE = %.4f" % mrse_hor1_vel + " | Avg Acceleration MRSE = %.4f" % mrse_hor1_acc)
+    print("2 second horizon || Avg Velocity MRSE = %.4f" % mrse_hor2_vel + " | Avg Acceleration MRSE = %.4f" % mrse_hor2_acc)
+    print("3 second horizon || Avg Velocity MRSE = %.4f" % mrse_hor3_vel + " | Avg Acceleration MRSE = %.4f" % mrse_hor3_acc)
+    print("4 second horizon || Avg Velocity MRSE = %.4f" % mrse_hor4_vel + " | Avg Acceleration MRSE = %.4f" % mrse_hor4_acc)
+    print("5 second horizon || Avg Velocity MRSE = %.4f" % mrse_hor5_vel + " | Avg Acceleration MRSE = %.4f" % mrse_hor5_acc)
 
     # Display inversions across all folds
-    avg_true_inversions = 1.0 * num_true_inversions / total_true_inversion_traj
-    avg_sim_inversions = 1.0 * num_sim_inversions / total_sim_inversion_traj
-    print("Avg True Jerk Inversions      = %.4f" % avg_true_inversions)
-    print("Avg Simulated Jerk Inversions = %.4f" % avg_sim_inversions)
+    avg_true_inversions = 1.0 * avg_true_inversions / count_true_inversion_traj
+    avg_sim_inversions = 1.0 * avg_sim_inversions / count_sim_inversion_traj
+    print("Avg True Jerk Inversions = %.4f" % avg_true_inversions + " | Avg Simulated Jerk Inversions = %.4f" % avg_sim_inversions)
 
     # Display negative values across all folds
-    avg_neg_dist = 1.0 * num_neg_dist / total_sim_traj
-    avg_neg_vel = 1.0 * num_neg_vel / total_sim_traj
-    print("Avg Total Negative Headway Distances = %.4f" % avg_neg_dist)
-    print("Avg Total Negative Velocities        = %.4f" % avg_neg_vel)
+    avg_neg_dist = 1.0 * avg_neg_dist / count_sim_traj
+    avg_neg_vel = 1.0 * avg_neg_vel / count_sim_traj
+    print("Avg Total Negative Headway Distances = %.4f" % avg_neg_dist + " | Avg Total Negative Velocities = %.4f" % avg_neg_vel)
 
 
 
@@ -450,7 +410,7 @@ if __name__ == "__main__":
     if input_type == "basic":
         traj_dir += "/basic_trajectories"
     elif input_type == "followers":
-        traj_dir += "/follower_02_basic_trajectories"
+        traj_dir += "/follower_01_basic_trajectories"
     elif input_type == "neighbors":
         traj_dir += "/neighbor_basic_trajectories"
         n = 21
@@ -460,26 +420,25 @@ if __name__ == "__main__":
 
 
     # Total simulated trajectories
-    total_sim_traj = 0
+    total_sim_traj = []
 
     # Error
-    vel_err = 0
-    acc_err = 0
+    vel_err = []
+    acc_err = []
 
     # Keep track of error at 1 to 5 second horizons
-    hor_vel_err = [0 for i in range(5)]
-    hor_acc_err = [0 for i in range(5)]
+    hor_vel_err = [[] for i in range(5)]
+    hor_acc_err = [[] for i in range(5)]
 
     # Inversions
-    num_true_inversions = 0
-    num_sim_inversions = 0
-    total_true_inversion_traj = 0
-    total_sim_inversion_traj = 0
-
+    num_true_inversions = []
+    num_sim_inversions = []
+    total_true_inversion_traj = []
+    total_sim_inversion_traj = []
 
     # Negatives
-    num_neg_dist = 0
-    num_neg_vel = 0
+    num_neg_dist = []
+    num_neg_vel = []
 
 
     ### Loop through 10 validation sets ###
@@ -493,12 +452,11 @@ if __name__ == "__main__":
             """ SEPARATE DATA """
             if input_type == "basic":
                 separated_data = separateData(f)
-                sim_dist, sim_rel, sim_vel, sim_acc, true_dist, true_rel, true_vel, true_acc = separated_data
+                sim_dist, sim_vel, sim_acc, true_dist, true_vel, true_acc = separated_data
 
             elif input_type == "followers":
                 separated_data = separateFollowerData(f)
-                sim_dist, sim_rel, sim_vel, sim_acc, sim_follow_dist, sim_follow_rel, \
-                true_dist, true_rel, true_vel, true_acc, true_follow_dist, true_follow_rel = separated_data
+                sim_dist, sim_vel, sim_acc, true_dist, true_vel, true_acc = separated_data
 
             elif input_type == "neighbors":
                 separated_data = separateNeighborsData(f)
@@ -506,40 +464,37 @@ if __name__ == "__main__":
 
 
             # Keep track of total simulated trajectories
-            total_sim_traj += len(sim_vel)
+            total_sim_traj.append(len(sim_vel))
 
             """ ERROR """
             # Keep track of MRSE before averaging for velocity and acceleration
             error_vals = calculateError(sim_vel, sim_acc, true_vel, true_acc)
-            vel_err += error_vals[0]
-            acc_err += error_vals[1]
+            vel_err.append(error_vals[0])
+            acc_err.append(error_vals[1])
             # Keep track of MRSE before averaging for each 1-5 sec time horizon
             for j in range(len(hor_vel_err)):
-                hor_vel_err[j] += error_vals[2][j]
-                hor_acc_err[j] += error_vals[3][j]
+                hor_vel_err[j].append(error_vals[2][j])
+                hor_acc_err[j].append(error_vals[3][j])
 
             """ JERK CALCULATIONS """
-            # Keep track of true jerk inversions
-            inversion_vals = countInversions(true_acc, "True")
-            num_true_inversions += inversion_vals[0]
-            total_true_inversion_traj += inversion_vals[1]
-
-            # Keep track of simulated jerk inversions
-            inversion_vals = countInversions(sim_acc, "Simulated")
-            num_sim_inversions += inversion_vals[0]
-            total_sim_inversion_traj += inversion_vals[1]
+            inversion_vals = countInversions(true_acc, sim_acc)
+            num_true_inversions.append(inversion_vals[0])
+            total_true_inversion_traj.append(inversion_vals[1])
+            num_sim_inversions.append(inversion_vals[2])
+            total_sim_inversion_traj.append(inversion_vals[3])
 
             """ NEGATIVE HEADWAY DISTANCE / VELOCITY """
             negative_vals = countNegatives(sim_dist, sim_vel, true_dist)
-            num_neg_dist += negative_vals[0]
-            num_neg_vel += negative_vals[1]
+            num_neg_dist.append(negative_vals[0])
+            num_neg_vel.append(negative_vals[1])
 
 
     """ Print statistics """
     printAnalysis(n-1, vel_err, acc_err, hor_vel_err, hor_acc_err, num_true_inversions, total_true_inversion_traj,
                   num_sim_inversions, total_sim_inversion_traj, num_neg_dist, num_neg_vel, total_sim_traj)
 
-
+    """ Output statistics to csv """
+    outputAnalysis()
 
 
 
